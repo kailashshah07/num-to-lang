c85b14c3aa248ae7e6fde06c157036666984a61d
Kailash <kailashshah07@gmail.com>
2023-10-27 20:31:55 +1100

    add names and comment

--- num-to-lang/Program.cs
+++ num-to-lang/Program.cs
@@ -1,4 +1,23 @@
-﻿namespace num_to_lang
+﻿/*
+
+Algorithm that converts a decimal number of less than nine million to an English language representation of that number.
+
+So for example:
+
+Given the input '3346780.54', the output should be "Three million, three hundred and forty six thousand, seven hundred and eighty, point fifty four.
+
+Steps for algorithm:
+    Step 1: check for decimal point in number.
+    Step 2: Split number on decimal point and call function to convert to string.
+    Step 3: In function to converto string take chunks of three digit from number and create strings of number based on their decimal position.
+    Step 4: Merge string of letter before and after decimal points based on if they have value before and after desimal position.
+    Step 5: Capitilise initial character.
+    Step 6: Print result.
+
+
+*/
+
+namespace num_to_lang
 {
     internal class Program
     {
@@ -44,6 +63,7 @@
             }
             else
             {
+                //seperating two digits number and converting
                 int onesDigit = number % 10;
                 int tensDigit = number / 10;
 
@@ -69,7 +89,7 @@
             }
         }
 
-        static string ConvertToWords(int number)
+        static string ConvertNumberToWords(int number)
         {
             string[] powerPosition = {
         ", ",
@@ -84,17 +104,20 @@
                 result = "Zero";
             else
             {
+                //dividing number into chunks of three and converting 
+
                 while (number / 100 > 0)
                 {
                     lastThreeDigits = number % 1000;
                     numAtHundred = lastThreeDigits / 100;
-                    result = twoDigitsToWords(numAtHundred) 
-                        + " hundred " + twoDigitsToWords(lastThreeDigits % 100) 
-                        + powerPosition[count] + result;
+                    result = twoDigitsToWords(numAtHundred) +
+                      " hundred " + twoDigitsToWords(lastThreeDigits % 100) +
+                      powerPosition[count] + result;
                     count++;
                     number /= 1000;
 
                 }
+                //converting if last chunk has less than three digits
                 if (number > 0)
                 {
                     result = twoDigitsToWords(number) + " " + powerPosition[count] + result;
@@ -106,17 +129,20 @@
 
         static string ConvertDecimalToWords(decimal number)
         {
+            //seperating number before and after decimal
             string[] parts = number.ToString("").Split('.');
             int integerPart = int.Parse(parts[0]);
             int fractionalPart = 0;
             if (parts.Length > 1)
                 fractionalPart = int.Parse(parts[1]);
 
-            string integerWords = ConvertToWords(integerPart);
-            string fractionalWords = ConvertToWords(fractionalPart);
+            string integerWords = ConvertNumberToWords(integerPart);
+            string fractionalWords = ConvertNumberToWords(fractionalPart);
+            //removing appending comma
             fractionalWords = fractionalWords.Remove(fractionalWords.Length - 2);
 
             string result = string.Empty;
+            //merging string based on value present before and after decimal
             if (integerPart != 0 && fractionalPart != 0)
                 result = integerWords + " point " + fractionalWords;
             else if (integerPart == 0 || fractionalPart != 0)
@@ -135,10 +161,10 @@
             if (decimal.TryParse(input, out decimal inputNumber))
             {
                 string words = ConvertDecimalToWords(inputNumber);
-                Console.WriteLine("English language representation of " 
-                    + inputNumber.ToString() + " is " 
-                    + words.First().ToString().ToUpper() 
-                    + String.Join("", words.Skip(1)));
+                Console.WriteLine("English language representation of " +
+                  inputNumber.ToString() + " is " +
+                  words.First().ToString().ToUpper() +
+                  String.Join("", words.Skip(1)));
             }
             else
             {
